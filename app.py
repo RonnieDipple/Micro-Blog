0ab23a0373e748a32a669152c84058fe55fa4562
Ronnie <ronniedipple@gmail.com>
2020-02-23 01:55:21 -0600

    Templates added

--- app.py
+++ app.py
@@ -8,7 +8,26 @@ app = Flask(__name__)
 @app.route('/index')
 def index ():
     user = {'username' : 'Ronnie'}
-    return render_template('index.html', title='Home', user=user)
+    posts = [
+
+        {
+            'author': {'username': 'Jack'},
+            'body': 'I am the anal sax batman'
+        },
+        {
+            'author': {'username' : 'John'},
+            'body': 'Beautiful day in portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool'
+
+        }
+
+
+
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
 
 
 if __name__ == '__main__':
