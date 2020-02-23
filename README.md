bda70c9c0fbd500b6b7ef3c18fe9045bdc99e51c
Ronnie <ronniedipple@gmail.com>
2020-02-22 23:21:45 -0600

    local host helloworld

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Micro-Blog
+This is a work in progress the aim is to create a python/flask microblogging site
---
+++ app.py
@@ -0,0 +1,15 @@
+from flask import Flask
+from flask import render_template
+
+app = Flask(__name__)
+
+
+@app.route('/')
+@app.route('/index')
+def index ():
+    user = {'username' : 'Ronnie'}
+    return render_template('index.html', title='Home', user=user)
+
+
+if __name__ == '__main__':
+    app.run()
