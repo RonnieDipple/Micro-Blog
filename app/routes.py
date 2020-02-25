1351cdec7b71a91c9fd46b377334840dad8d78b4
Ronnie <ronniedipple@gmail.com>
2020-02-25 00:59:20 -0600

    Login web forms added

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,12 @@
-from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
+from flask import render_template, flash, redirect, url_for
+from app import app
+from app.forms import LoginForm
 
 
 @app.route('/')
 @app.route('/index')
-def index ():
-    user = {'username' : 'Ronnie'}
+def index():
+    user = {'username': 'Ronnie'}
     posts = [
 
         {
@@ -15,7 +14,7 @@ def index ():
             'body': 'I am the anal sax batman'
         },
         {
-            'author': {'username' : 'John'},
+            'author': {'username': 'John'},
             'body': 'Beautiful day in portland!'
         },
         {
@@ -23,12 +22,15 @@ def index ():
             'body': 'The Avengers movie was so cool'
 
         }
-
-
-
     ]
     return render_template('index.html', title='Home', user=user, posts=posts)
 
 
-if __name__ == '__main__':
-    app.run()
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash('Login requested for user {}, remember_me={}'.format(
+            form.username.data, form.remember_me.data))
+        return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
